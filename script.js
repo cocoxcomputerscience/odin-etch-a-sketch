a7f876024e11eb99a5ef4f646bc3edcf75d04ebe
coco <cocoxcomputerscience@gmail.com>
2023-06-02 17:07:05 -0400

    Finish the javascript file excluding refactoring
    This update includes:
    - a colorpicker
    - finished functionality of the customization functions
    
    What is needed still:
    - styling
    - responsive web design

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// to initially populate grid container
-let gridContainer = document.querySelector("#grid-container");
-
 function populateGrid(size = 16) {
     // will delete all children nodes if grid size is updated
     if (gridContainer.hasChildNodes()) gridContainer.innerHTML = "";
@@ -20,93 +17,102 @@ function populateGrid(size = 16) {
     }
 }
 
-populateGrid();
-
-// change color of gridItem when mouse is down and over gridItem
-let mouseDown = false;
-document.body.addEventListener("mousedown", () => {mouseDown = true;});
-document.body.addEventListener("mouseup", () => {mouseDown = false;})
-
-//let gridItems = document.querySelectorAll(".grid-item");
-//gridItems.forEach(gridItem => gridItem.addEventListener("mouseover", addGridItemColor));
-//gridItems.forEach(gridItem => gridItem.addEventListener("mousedown", addGridItemColor));
-document.body.addEventListener("mouseover", addGridColor);
-document.body.addEventListener("mousedown", addGridColor);
-let gridColor = "black";
-
-function addGridColor(e) {
+function addColor(e) {
     // event delegation is used for the dynamically created grid
     const target = e.target.closest(".grid-item");
 
     if (target) {
-        if (e.type === 'mouseover' && mouseDown === false) {
-            return
+        if (e.type === "mouseover" && mouseDownFlag === false) return;
+
+        // checking eraser and rainbow flags
+        if (eraserFlag === true) {
+            target.style.backgroundColor = "white";
+            return;
+        } else if (rainbowFlag === true) {
+            target.style.backgroundColor = getRainbowColor();
+            return;
+        } else {
+            target.style.backgroundColor = gridColor; 
         }
-        // !!! need to change to selected color later
-        target.style.backgroundColor = gridColor; 
     }
 }
 
-// grid size slider
-let gridSizeToggle = document.querySelector("#grid-size-toggle");
-let gridSize = document.querySelector("#grid-size");
+function toggleRainbow() {
+    // need to toggle eraser button if it is clicked
+    if (eraserFlag) toggleEraser();
 
-gridSizeToggle.addEventListener("input", updateGridSize);
-
-function updateGridSize(e) {
-    let gridSizeValue = this.value;
-    // updating the displayed grid size underneath the range button
-    gridSize.textContent = `${gridSizeValue} x ${gridSizeValue}`;
-
-    // updating the acutal grid size 
-    populateGrid(gridSizeValue);
+    rainbowFlag = !rainbowFlag;
+    rainbowButton.classList.toggle("clicked-button");
 }
 
-// clear button
-let clearButton = document.body.querySelector("#clear-button");
-clearButton.addEventListener("click", clearGrid);
+function getRainbowColor() {
+    let rainbowColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return rainbowColor;
+}
+
+function toggleGridLines(newGridSize = false) {
+    let gridContainerItems = gridContainer.childNodes;
+
+    // if statement will run if function is called from updateGridSize
+    // checks to see if grid lines button is clicked. If clicked, then re-adds grid lines
+    if (newGridSize) {
+        if (gridLinesButton.classList.contains("clicked-button")) {
+            gridContainerItems.forEach(gridItem => gridItem.classList.toggle("grid-lines"));       
+        }
+        return;
+    }
+
+    gridLinesButton.classList.toggle("clicked-button");
+    gridContainerItems.forEach(gridItem => gridItem.classList.toggle("grid-lines"));
+}
+
+function toggleEraser() {
+    // need to toggle rainbow button if it is clicked
+    if (rainbowFlag) toggleRainbow();
+
+    eraserFlag = !eraserFlag;
+    eraserButton.classList.toggle("clicked-button");
+}
 
 function clearGrid() {
     let gridContainerItems = gridContainer.childNodes;
     gridContainerItems.forEach(gridItem => gridItem.style.backgroundColor = "white");
-
-    // defaults
-    gridColor = "black";
-    eraserToggle = false;
-
-    // !!! need to add defaults of changing buttons back to unclicked states 
-    // !!! did not add css style for that yet
 }
 
-// eraser button
-let eraserToggle = false;
-let eraserButton = document.body.querySelector("#eraser-button");
-eraserButton.addEventListener("click", toggleEraser);
+function updateGridSize(e) {
+    let gridSizeValue = e.target.value;
+    
+    // updating the displayed grid size underneath the range button
+    gridSize.textContent = `${gridSizeValue} x ${gridSizeValue}`;
 
-function toggleEraser() {
-    // code to make color white
-    if (!eraserToggle) {
-        gridColor = "white";
-        eraserToggle = true;
-        return;
-    }
-    // !!! need to change to selected color later
-    gridColor = "black";
-    eraserToggle = false;
+    // updating the actual grid size 
+    populateGrid(gridSizeValue);
+    toggleGridLines(true);
 }
 
-// add grid lines
+let mouseDownFlag = false;
+let gridColor = "black";
+let rainbowFlag = false;
+let eraserFlag = false;
+let gridContainer = document.querySelector("#grid-container");
+let colorpicker = document.body.querySelector("#colorpicker");
+let rainbowButton = document.body.querySelector("#rainbow-button");
 let gridLinesButton = document.body.querySelector("#grid-lines-button");
-gridLinesButton.addEventListener("click", toggleGridLines);
+let eraserButton = document.body.querySelector("#eraser-button");
+let clearButton = document.body.querySelector("#clear-button");
+let gridSizeToggle = document.querySelector("#grid-size-toggle");
+let gridSize = document.querySelector("#grid-size");
 
-function toggleGridLines() {
-    let gridContainerItems = gridContainer.childNodes;
+document.body.addEventListener("mousedown", () => {mouseDownFlag = true;});
+document.body.addEventListener("mouseup", () => {mouseDownFlag = false;});
+document.body.addEventListener("mousedown", addColor);
+document.body.addEventListener("mouseover", addColor);
+colorpicker.addEventListener("input", () => gridColor = colorpicker.value);
+rainbowButton.addEventListener("click", toggleRainbow);
+// arrow function is used so that the default event argument is not passed
+gridLinesButton.addEventListener("click", () => {toggleGridLines();});
+eraserButton.addEventListener("click", toggleEraser);
+clearButton.addEventListener("click", clearGrid);
+gridSizeToggle.addEventListener("input", updateGridSize);
 
-    // checking if one child has grid-lines
-    if (gridContainerItems[0].classList.contains("grid-lines")) {
-        gridContainerItems.forEach(gridItem => gridItem.classList.remove("grid-lines"));
-        return;
-    }
-
-    gridContainerItems.forEach(gridItem => gridItem.classList.add("grid-lines"));
-}
+populateGrid();
